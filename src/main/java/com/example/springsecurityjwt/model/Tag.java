e6938530a2cfef011cf56cd62c99373e01646f74
tobey <fafoworatobi25@gmail.com>
2022-05-06 14:18:34 +0100

    tag db setup

---
+++ src/main/java/com/example/springsecurityjwt/model/Tag.java
@@ -0,0 +1,19 @@
+package com.example.springsecurityjwt.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Tag extends BaseEntity{
+
+    public Tag(long id){
+        super(id);
+    }
+
+    private String title;
+    private String slug;
+}
